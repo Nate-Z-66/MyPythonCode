d4110b9809f00c81eabe9fd46a4823702d1c2f35
Nate-Z-66 <114251966+Nate-Z-66@users.noreply.github.com>
2022-09-25 21:59:35 -0700

    Update 1st_Python_Game.py

--- 1st_Python_Game.py
+++ 1st_Python_Game.py
@@ -2,7 +2,7 @@ import random
 Player = int(input("rock,1  paper,2  scissors,3: "))
 Computer = random.randint(1, 3)
 
-if((Player > Computer) or (Player == 1 and Computer == 3)):
+if((Player == 1 and Computer == 3) or (Player == 2 and Computer == 1) or (Player == 3 and Computer == 2)) :
         print ("Player Wins")
         print(Computer)
 elif Player == Computer :
